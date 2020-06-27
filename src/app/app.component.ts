af7b970d005caa8ef1baacd2a84866332a546d5f
Indraraj Yadav <indraraj@infiniteit.biz>
2020-06-27 15:06:38 +0530

    chore: back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,46 @@
 import { Component } from '@angular/core';
 
-import { Platform, NavController } from '@ionic/angular';
+import { Platform, NavController, IonRouterOutlet } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { UserDetailService } from './services/user-detail.service';
+import { AppMinimize } from '@ionic-native/app-minimize/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private _navController: NavController,
-    private _userDetailService: UserDetailService,
-  ) {
-    this.initializeApp();
-  }
+	constructor(
+		private platform: Platform,
+		private splashScreen: SplashScreen,
+		private statusBar: StatusBar,
+		private _navController: NavController,
+		private _userDetailService: UserDetailService,
+		private _ionRouterOutlet: IonRouterOutlet,
+		private _app: AppMinimize,
+	) {
+		this.initializeApp();
+	}
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+	initializeApp() {
+		this.platform.ready().then(() => {
+			this.statusBar.styleDefault();
+			this.splashScreen.hide();
+			this.platform.backButton.subscribeWithPriority(-1, async (_) => {
+				console.log(
+					this._ionRouterOutlet.component,
+				);
+				if (!this._ionRouterOutlet.canGoBack()) {
+					await this._app.minimize();
+				}
+			});
+		});
+	}
 
-  onLogout(): void {
-    this._userDetailService.clearUserData();
-    this._navController.navigateRoot('/auth');
-  }
+	onLogout(): void {
+		this._userDetailService.clearUserData();
+		this._navController.navigateRoot('/auth');
+	}
 }
