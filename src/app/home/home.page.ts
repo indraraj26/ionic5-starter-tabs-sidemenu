001690c8fba02f67b6e020d5bf25e2d755842445
Indraraj Yadav <indraraj@infiniteit.biz>
2020-08-30 11:59:08 +0530

    feat: picker init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
 	selector: 'app-home',
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-	public loginDetails: any = { phone_no: '+91 - ' };
-	constructor() {}
+	public timeSlot: any[] = [
+		{
+			name: 'Timeslot',
+			options: [
+				{
+					text: '10am-11am',
+					value: 1,
+				},
+				{
+					text: '11am-12pm',
+					value: 2,
+				},
+				{
+					text: '1pm-2pm',
+					value: 3,
+				},
+				{
+					text: '2pm-3pm',
+					value: 4,
+				},
+				{
+					text: '4pm-5pm',
+					value: 5,
+				},
+			],
+		},
+	];
+
+	constructor(private _picker: PickerController) {}
 
 	ngOnInit() {}
 
-	onSubmit(f) {}
+	async openPicker() {
+		const picker = await this._picker.create({
+			columns: this.timeSlot,
+			buttons: [
+				{
+					text: 'Cancel',
+					role: 'cancel',
+				},
+				{
+					text: 'Confirm',
+					handler: (value) => {
+						console.log('confirmed pressed', value);
+					},
+				},
+			],
+		});
+
+		picker.present();
+	}
 }
