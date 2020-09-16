bc00b2f3713e5c2f6a51951f7fa428765e409698
Indraraj Yadav <indraraj@infiniteit.biz>
2020-09-16 15:26:03 +0530

    feat: keyboard press

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,22 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { UserDetailService } from '../services/user-detail.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+	selector: 'app-auth',
+	templateUrl: './auth.page.html',
+	styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-  constructor(
-    private _navController: NavController,
-    private userDetailService: UserDetailService,
-  ) {}
+	@HostListener('keydown', ['$event']) onKeyup(event: any) {
+		console.log(event, 'hostlistern');
+		console.log(event.which, 'hostlistern');
+	}
+	constructor(
+		private _navController: NavController,
+		private userDetailService: UserDetailService,
+	) {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 
-  onLogin() {
-    this.userDetailService.setUserData();
-    this._navController.navigateRoot('/tabs/dashboard');
-  }
+	onLogin() {
+		this.userDetailService.setUserData();
+		this._navController.navigateRoot('/tabs/dashboard');
+	}
+
+	onKeyUp(e) {
+		console.log(e);
+	}
+	onKeyDown(e) {
+		console.log(e);
+	}
+	onKeyPress(e) {
+		console.log(e);
+	}
+	onHtmlTextArea(e) {
+		console.log(e, 'htmltext');
+	}
 }
